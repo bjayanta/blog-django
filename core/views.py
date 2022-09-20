790f974338fcf4a2d4eccfbeafe699333d876612
Jayanta <bjayanta.me@gmail.com>
2022-09-20 18:18:12 +0600

    add pagination

--- core/views.py
+++ core/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 from website.models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
 class IndexView(View):
     meta = {
         "title": "Index",
-        "posts": Post.objects.filter(status = Post.ACTIVE)
     }
 
     def get(self, request):
+        posts = Post.objects.filter(status = Post.ACTIVE)
+        page = Paginator(posts, 10)
+
+        self.meta['posts'] = page.get_page(request.GET.get('page'))
+
         return render(request, 'core/index.html', self.meta)
 
 class AboutView(View):
